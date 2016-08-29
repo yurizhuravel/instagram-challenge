1fff663fb460fc62e71bde541b62a47ba4310325
yurizhuravel <zhuravel@gmail.com>
2016-08-29 13:35:21 +0100

    Adds tests on comments functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :snaps
+  resources :snaps do
+    resources :comments
+  end
 
   root 'snaps#index'
 
---
+++ spec/features/comments_feature_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require 'web_helper'
+
+feature 'Leaving comments' do
+  before do
+    sign_up
+    add_snap
+  end
+
+  scenario 'allows users to leave a comment on a snap' do
+     visit '/'
+     click_link 'Add a comment'
+     fill_in "Your Comment", with: "wow wow!"
+     click_button 'Post comment'
+
+     expect(current_path).to eq '/snaps'
+     expect(page).to have_content('wow wow!')
+  end
+
+end
